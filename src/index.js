7d5fc8bf7b649223387d1e8ab73c7d5057ae585c
pawan panth <pawan.panth.pp@gmail.com>
2020-11-28 00:27:39 +0530

    m

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const httpServer = http.createServer(handleServer).listen(8081);
 function handleServer(req, res) {
     
     // console.log(res);
-    if(req.url=="/welcome"){
+    if(req.url.startswith("/welcome") && req.method == "GET"){
         res.writeHead(200,{'content-Type':'text/plain'});
         res.write('Welcome to Dominos!');
         res.end();
     }
 
-    else if(req.url=="/contact"){
+    else if(req.url.startswith("/contact") && req.method == "GET"){
         res.writeHead(200,{'content-Type':'application/json'});
         res.write(JSON.stringify({
             phone: '18602100000',
